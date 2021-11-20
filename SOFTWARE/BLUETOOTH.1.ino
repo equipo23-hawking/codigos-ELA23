b16cf0290e8f247700610eb281bb8bd86e52100f
MariaRejas <89707896+MariaRejas@users.noreply.github.com>
2021-11-19 23:08:04 -0500

    Update BLUETOOTH.1.ino

--- SOFTWARE/BLUETOOTH.1.ino
+++ SOFTWARE/BLUETOOTH.1.ino
@@ -1,10 +1,8 @@
 // Modulo de Bluetooth
 
-#include <SoftwareSerial.h>
-SoftwareSerial Bt serial(2,3);
+#include <SoftwareSerial.h>;
 //Conectar el HC-05 TX al pin 2 RX del arduino nano
-//Conectar el HC-05 RX al pin 3 TX del arduino UNO, por medio de un divisor de tension
-char c=' ';
+//Conectar el HC-05 RX al pin 3 TX del arduino UNO, por medio de un divisor de tension;
 asdfghj
 void setup(){
   Serial.begin(9600); //Inicializamos el puerto de serie
